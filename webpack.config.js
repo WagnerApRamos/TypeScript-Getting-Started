750d5b3bb2c7ebfc501f99ccd12ce21c122cbd94
Wagner Ramos <wagner.ap.ramos@gmail.com>
2019-11-26 00:03:57 +0000

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   entry: './app/app.ts',
   devtool: 'inline-source-map',
+  module: {
+    rules :[
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /module_nodes/
+      }
+    ]
+  },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
